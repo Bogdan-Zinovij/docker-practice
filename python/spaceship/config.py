6e7f39a16d40d447959eb1be94003bbbcd0fc787
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-20 15:24:51 +0300

    Fix build error

--- python/spaceship/config.py
+++ python/spaceship/config.py
@@ -5,6 +5,6 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'spaceship'
-    app_description: str = 'Laboratory assignment 3, Bogdan Zinovij'
+    app_description: str = 'Laboratory assignment 03, Bogdan Zinovij'
     app_version: str = 'dev'
     debug: bool = False
