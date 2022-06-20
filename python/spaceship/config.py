a3301f6e580596feb60664eeaca747362e8c825f
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-20 14:35:30 +0300

    Make some changes in output string

--- python/spaceship/config.py
+++ python/spaceship/config.py
@@ -5,6 +5,6 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'spaceship'
-    app_description: str = 'Laboratory assignment 03 starter project for software engineering course'
+    app_description: str = 'Laboratory assignment 03, Bogdan Zinovij'
     app_version: str = 'dev'
     debug: bool = False
